c11ea600a7f23d69135681af65abd0d7e7df1603
Hamza <hamzaargoubi@hotmail.com>
2020-05-02 02:40:17 +0100

    commit1

---
+++ src/edu/mawhebti/gui/Login1Controller.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.mawhebti.gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author Fakhri Argoubi
+ */
+public class Login1Controller implements Initializable {
+
+    @FXML
+    private Button tfSignin;
+    @FXML
+    private TextField tfLogin;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    private Login1Controller(ActionEvent event) {
+    }
+
+    @FXML
+    private Login1Controller(ActionEvent event) {
+    }
+    
+}
