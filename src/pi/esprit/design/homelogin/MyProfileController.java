97cbe4e649873d05e01c6ed43a3ca21238419f60
Hamza <hamzaargoubi@hotmail.com>
2020-05-30 13:11:23 +0100

    30/5/v3/

--- src/pi/esprit/design/homelogin/MyProfileController.java
+++ src/pi/esprit/design/homelogin/MyProfileController.java
@@ -5,9 +5,12 @@
  */
 package pi.esprit.design.homelogin;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
+import java.util.UUID;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.event.ActionEvent;
@@ -16,13 +19,19 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import pi.esprit.entities.InputValidation;
 import pi.esprit.entities.loggedmembre;
 import pi.esprit.entities.personnes;
 import pi.esprit.services.MailService;
@@ -71,7 +80,11 @@ public class MyProfileController implements Initializable {
     private Label sqlFirstNameLb;
     @FXML
     private Button changePwdBtn;
-
+    
+    Image image ;
+    String photo;
+    @FXML
+    private Text textImage;
     /**
      * Initializes the controller class.
      */
@@ -132,7 +145,38 @@ public class MyProfileController implements Initializable {
     }
 
     @FXML
-    private void changeAvatar(ActionEvent event) {
+    private void changeAvatar(ActionEvent event) throws IOException {
+        FileChooser fileChooser = new FileChooser();
+        final Stage stage = new Stage();
+        File file = fileChooser.showOpenDialog(stage);
+        System.out.println(file);
+        
+        if (file != null) {
+            String p = file.toURI().toString();
+            System.out.println(p);
+            
+            photo = UUID.randomUUID().toString().replaceAll("-", "") + ".jpg";
+            image = new Image(file.getAbsoluteFile().toURI().toString(), imgviewer.getFitWidth(), imgviewer.getFitHeight(), true, true);
+            // txt_vid.setText(video);
+            textImage.setText(photo);
+            InputValidation u = new InputValidation();
+            String image1;
+            image1 = "C:\\wamp64\\www\\mawhebti\\images\\" + photo;
+            System.out.println(photo);
+            imgviewer.setImage(image);
+           
+            Alert alert1 = new Alert(Alert.AlertType.CONFIRMATION);
+            alert1.setTitle("Saving image");
+            alert1.setHeaderText("Information");
+            alert1.setContentText("do you want save this image?");
+            Optional<ButtonType> action = alert1.showAndWait();
+            if (action.get() == ButtonType.OK) {
+                
+                u.CopyImage(image1, file.toPath().toString());
+                
+                
+            }
+        }
     }
     
 }
